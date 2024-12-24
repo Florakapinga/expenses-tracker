7bd0ecc4b93978ffcc37f0c05763205f6b87e98e
kapinga <erickfloriana@gmail.com>
2024-12-24 12:58:22 -0800

    creating expenses function This class serves as a blueprint for creating expense objects

--- expenses.cpp
+++ expenses.cpp
@@ -2,3 +2,11 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+class Expense
+{
+public:
+    std::string category;
+    double amount;
+
+    Expense(std::string cat, double amt) : category(cat), amount(amt) {}
+};
